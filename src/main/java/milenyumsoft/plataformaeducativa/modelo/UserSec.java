493fee7e1f99f6fd7c5eade06c80be403d8e80ec
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-03-17 18:15:05 -0500

    Integración de atributos para el modelo UserSec

--- src/main/java/milenyumsoft/plataformaeducativa/modelo/UserSec.java
+++ src/main/java/milenyumsoft/plataformaeducativa/modelo/UserSec.java
@@ -3,6 +3,9 @@ package milenyumsoft.plataformaeducativa.modelo;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Setter @Getter
 @AllArgsConstructor @NoArgsConstructor @ToString
@@ -14,6 +17,15 @@ public class UserSec {
     private Long id;
     @Column(unique = true)
     private String username;
+    private String password;
+    private boolean enabled;
+    private boolean accountNotExpired;
+    private boolean accountNotLocked;
+    private boolean credentialNotExpired;
+
+
+    private Set<Role> roleList = new HashSet<>();
+
 
 
 
