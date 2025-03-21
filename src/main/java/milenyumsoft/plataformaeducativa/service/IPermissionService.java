90df5673180fa4497b455e77c85ecbc00e2fb1f9
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-03-21 18:53:42 -0500

    Integración de archivos role, permission

--- src/main/java/milenyumsoft/plataformaeducativa/service/IPermissionService.java
+++ src/main/java/milenyumsoft/plataformaeducativa/service/IPermissionService.java
@@ -1,4 +1,15 @@
 package milenyumsoft.plataformaeducativa.service;
 
+import milenyumsoft.plataformaeducativa.modelo.Permission;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IPermissionService {
+
+    List<Permission> findAll();
+    Optional<Permission> findById(Long id);
+    Permission save(Permission permission);
+    void deleteById(Long id);
+    Permission update(Permission permission);
 }
--- src/main/java/milenyumsoft/plataformaeducativa/service/IRoleService.java
+++ src/main/java/milenyumsoft/plataformaeducativa/service/IRoleService.java
@@ -1,4 +1,15 @@
 package milenyumsoft.plataformaeducativa.service;
 
+import milenyumsoft.plataformaeducativa.modelo.Role;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IRoleService {
+
+
+    List<Role> findAll();
+    Optional<Role> findById(Long id);
+    Role save(Role role);
+    void deleteById(Long id);
 }
