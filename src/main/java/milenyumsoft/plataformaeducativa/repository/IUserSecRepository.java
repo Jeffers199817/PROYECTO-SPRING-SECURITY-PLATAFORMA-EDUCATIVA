ceb1df42c151db229e6e031c5c9d8ac8fbd4a418
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-03-25 16:43:12 -0500

    Integración de userDetailsService

--- src/main/java/milenyumsoft/plataformaeducativa/repository/IUserSecRepository.java
+++ src/main/java/milenyumsoft/plataformaeducativa/repository/IUserSecRepository.java
@@ -4,6 +4,11 @@ import milenyumsoft.plataformaeducativa.modelo.UserSec;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUserSecRepository extends JpaRepository<UserSec, Long> {
+
+
+    Optional<UserSec> findUserEntityByUsername(String name);
 }
---
+++ src/main/java/milenyumsoft/plataformaeducativa/service/UserDetailsServiceImp.java
@@ -0,0 +1,58 @@
+package milenyumsoft.plataformaeducativa.service;
+
+import milenyumsoft.plataformaeducativa.modelo.UserSec;
+import milenyumsoft.plataformaeducativa.repository.IUserSecRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    private IUserSecRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+
+        //1.- Traemos el usuario sec y devolvemos en userdetails
+
+        UserSec userSec = userRepo.findUserEntityByUsername(username)
+                .orElseThrow(()->new UsernameNotFoundException("El usuario " + username + " no fue encontrado"));
+
+        //2.- Creamos un instancia de grantedAuthority
+
+        List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+
+        //3.- Programación funcional tomamos los roles y los converitmos en simplegrantedauthority para poder agregarlos a la authoritylist.
+
+        userSec.getRoleList()
+                .forEach(role->authorityList.add(new SimpleGrantedAuthority("ROLE_".concat(role.getRole()))));
+
+        //4. Pasar a grantedauthority a los permissions
+
+        userSec.getRoleList().stream()
+                .flatMap(role->role.getPermissionsList().stream())
+                .forEach(permission ->authorityList.add(new SimpleGrantedAuthority(permission.getPermissionName())) );
+
+        //5. retornamos el usaurio en formato spring security con loedd datos de nuestro usersec
+
+
+        return new User(
+                userSec.getUsername(),
+                userSec.getPassword(),
+                userSec.isEnabled(),
+                userSec.isAccountNotExpired(),
+                userSec.isCredentialNotExpired(),
+                userSec.isAccountNotLocked(),
+                authorityList);
+    }
+}
