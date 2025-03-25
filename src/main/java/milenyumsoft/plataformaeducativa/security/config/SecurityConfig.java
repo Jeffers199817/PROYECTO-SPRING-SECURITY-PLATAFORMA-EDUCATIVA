4b4eb31c1c59430fc22f59e135c14bcb4bb15f16
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-03-25 13:31:56 -0500

    Integración de Security config

--- src/main/java/milenyumsoft/plataformaeducativa/security/config/SecurityConfig.java
+++ src/main/java/milenyumsoft/plataformaeducativa/security/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package milenyumsoft.plataformaeducativa.security.config;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -45,8 +53,36 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(csrf->csrf.disable())
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement((session)-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    //2. Creamos un authentication manager
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+            return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //3.Crear un metodo para el proveedor de autenticación
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService){
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider;
 
     }
+
+    // 4. Creamos nuestro passwrodEncdder
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 }
